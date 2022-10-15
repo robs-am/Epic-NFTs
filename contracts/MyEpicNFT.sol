0064ea7b2f049318ad3c99079c6f983c83c7901c
robs-am <roberta.amaro89@gmail.com>
2022-10-14 22:57:29 -0300

    add contracts and scripts

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0; //versao do compilador solidity
+import "hardhat/console.sol";
+contract MyEpicNFT {
+   constructor() {
+      console.log("Esse eh meu contrato de NFT! Tchu-hu");
+   }
+}
